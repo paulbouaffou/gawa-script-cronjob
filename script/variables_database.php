b184245a5aed554acc4f4d1839d86b704953c867
Paboko <bkodossoubabapaul@gmail.com>
2023-07-28 10:52:43 +0000

    Touch and Add content file VARIABLES_DATABASE.PHP

---
+++ script/variables_database.php
@@ -0,0 +1,7 @@
+<?php
+
+# VARIABLES  DE CONNEXION A LA BASE DE DONNEES DE L'OUTIL GAWA.
+
+$db_connection = 'mysql:host=localhost;dbname=gawadb;charset=utf8';
+$username = 'root';
+$password = ''; 
